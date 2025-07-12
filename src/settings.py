10b51ef27a18e98c3640908ea956c71cbf730db6
osushinekotan <spmt3710@gmail.com>
2025-07-12 22:35:27 +0900

    add: enhance KaggleSettings with code name and handle fields

--- src/settings.py
+++ src/settings.py
@@ -9,6 +9,10 @@ DATA_DIR = Path(ROOT_DIR) / "data"
 INPUT_DIR = DATA_DIR / "input"
 OUTPUT_DIR = DATA_DIR / "output"
 
+CODES_DIR = ROOT_DIR / "codes"
+DEPS_CODE_DIR = CODES_DIR / "deps"
+SUBMISSION_CODE_DIR = CODES_DIR / "submission"
+
 
 class KaggleSettings(BaseSettings):
     """
@@ -22,10 +26,19 @@ class KaggleSettings(BaseSettings):
     BASE_ARTIFACTS_HANDLE: str = Field("", description="Base handle for Kaggle artifacts.")
     CODES_HANDLE: str = Field("", description="Handle for Kaggle codes.")
 
+    DEPS_CODE_NAME: str = Field("", description="Name of the Deps Kaggle code.")
+    SUBMISSION_CODE_NAME: str = Field("", description="Name of the Submission Kaggle code.")
+
     @model_validator(mode="after")
     def set_handles(self):
         self.CODES_HANDLE = f"{self.KAGGLE_USERNAME}/{self.KAGGLE_COMPETITION_NAME}-codes"
         self.BASE_ARTIFACTS_HANDLE = f"{self.KAGGLE_USERNAME}/{self.KAGGLE_COMPETITION_NAME}-artifacts/other"
+        return self
+
+    @model_validator(mode="after")
+    def set_code_name(self):
+        self.DEPS_CODE_NAME = f"{self.KAGGLE_COMPETITION_NAME}-deps"
+        self.SUBMISSION_CODE_NAME = f"{self.KAGGLE_COMPETITION_NAME}-submission"
         return self
 
     model_config = SettingsConfigDict(
